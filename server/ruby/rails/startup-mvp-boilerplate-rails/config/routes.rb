418a170c84a30cd92b10df0d7e09f0a8dfa18535
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-03-26 16:15:13 -0300

    Adding main routes.

--- server/ruby/rails/startup-mvp-boilerplate-rails/config/routes.rb
+++ server/ruby/rails/startup-mvp-boilerplate-rails/config/routes.rb
@@ -1,25 +1,26 @@
 Rails.application.routes.draw do
-  get 'user/dashboard'
+  root to: "public#landing"
 
-  get 'development/dashboard'
-
-  get 'administration/dashboard'
-
-  namespace :public do
-    get 'error/not_found'
+  namespace :administration do
+    get 'dashboard'
   end
 
-  get 'public/about'
+  namespace :development do
+    get 'dashboard'
+  end
 
-  get 'public/careers'
+  get 'about', to: 'public#about'
+  get 'careers', to: 'public#careers'
+  get 'help', to: 'public#help'
+  get 'landing', to: 'public#landing'
+  get 'privacy_policy', to: 'public#privacy_policy'
+  get 'sitemap', to: 'public#sitemap'
+  get 'terms_of_service', to: 'public#terms_of_service'
+  get 'error/not_found', to: 'public/error#not_found'
 
-  get 'public/help'
-
-  get 'public/privacy_policy'
-
-  get 'public/terms_of_service'
-
-  get 'public/landing'
+  namespace :user do
+    get 'dashboard'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
