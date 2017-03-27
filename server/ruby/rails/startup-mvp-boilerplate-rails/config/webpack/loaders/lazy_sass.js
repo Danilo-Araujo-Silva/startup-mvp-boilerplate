c672db33d8ecf780188f09f884908cefd406a630
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-03-27 16:44:30 -0300

    Very interesting improvement to load css/scss files without relative path and still working on the material components web legacy problem.

--- server/ruby/rails/startup-mvp-boilerplate-rails/config/webpack/loaders/lazy_sass.js
+++ server/ruby/rails/startup-mvp-boilerplate-rails/config/webpack/loaders/lazy_sass.js
@@ -1,8 +1,16 @@
+var path = require('path');
+let root = path.resolve(__dirname, '../../../', 'node_modules');
+
 module.exports = {
   test: /\.useable\.(scss|sass|css)$/i,
   use: [
     {loader: "style-loader/useable"},
     {loader: "css-loader"},
-    {loader: "sass-loader"},
+    {
+      loader: "sass-loader",
+      options: {
+        includePaths: [root]
+      }
+    },
   ],
 }
