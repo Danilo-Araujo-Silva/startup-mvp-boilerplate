6b8d0de0ddf4c04713837124d1522f0e4d44c0a7
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-03-29 14:57:11 -0300

    Adding route for devise.

--- server/ruby/rails/startup-mvp-boilerplate-rails/config/routes.rb
+++ server/ruby/rails/startup-mvp-boilerplate-rails/config/routes.rb
@@ -22,7 +22,9 @@ Rails.application.routes.draw do
     get 'dashboard'
   end
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users_controller/omniauth_callbacks" }
+
+  # resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
