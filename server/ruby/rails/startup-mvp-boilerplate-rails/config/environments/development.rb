f2014fe4c62d74dfe78094c1755071ddcbb44b6e
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-03-29 14:54:43 -0300

    Adding mailer configurations for devise.

--- server/ruby/rails/startup-mvp-boilerplate-rails/config/environments/development.rb
+++ server/ruby/rails/startup-mvp-boilerplate-rails/config/environments/development.rb
@@ -31,8 +31,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Needed for devise.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Mailer default url for Devise.
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.perform_deliveries    = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options   = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
