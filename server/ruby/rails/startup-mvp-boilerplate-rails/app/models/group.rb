0c9c1bb26e9303b4f5fca1da1388bb662d430e7c
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-03-31 16:31:02 -0300

    Adding breacrumb methods to groups.

--- server/ruby/rails/startup-mvp-boilerplate-rails/app/models/group.rb
+++ server/ruby/rails/startup-mvp-boilerplate-rails/app/models/group.rb
@@ -2,7 +2,8 @@ class Group < ApplicationRecord
   has_many :children, :class_name => Group.name, :foreign_key => "parent_id"
   belongs_to :parent, :class_name => Group.name, :foreign_key => "parent_id", optional: true
 
-  #TODO Add breadcrumb methods.
+  @@breadcrumb_by_ids_separator = '.'
+  @@breadcrumb_by_titles_separator = ' > '
 
   @@main_groups = {
     root: Group.find_by_title('root'),
@@ -28,5 +29,25 @@ class Group < ApplicationRecord
 
   def _validate
     super
+  end
+
+  def breadcrumb_by_ids
+    return @breadcrumb_by_ids if defined? @breadcrumb_by_ids
+
+    @breadcrumb_by_ids = self.id if self.parent_id.nil?
+
+    @breadcrumb_by_ids = "#{self.parent.breadcrumb_by_ids}#{@@breadcrumb_by_ids_separator}#{self.id}" if !self.parent_id.nil?
+
+    @breadcrumb_by_ids
+  end
+
+  def breadcrumb_by_titles
+    return @breadcrumb_by_titles if defined? @breadcrumb_by_titles
+
+    @breadcrumb_by_titles = self.title if self.parent_id.nil?
+
+    @breadcrumb_by_titles = "#{self.parent.breadcrumb_by_titles}#{@@breadcrumb_by_titles_separator}#{self.title}" if !self.parent_id.nil?
+
+    @breadcrumb_by_titles
   end
 end
