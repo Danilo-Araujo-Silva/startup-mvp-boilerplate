180a113132a3b29ef8a72d654f69f68c99836cf0
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-04-01 10:00:42 -0300

    Changing webpack loaders sass and useable sass.

--- server/ruby/rails/startup-mvp-boilerplate-rails/config/webpack/loaders/sass.js
+++ server/ruby/rails/startup-mvp-boilerplate-rails/config/webpack/loaders/sass.js
@@ -1,10 +1,26 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+var path = require('path');
+let assets_stylesheets = path.resolve(__dirname, '../../../', 'app', 'assets', 'stylesheets');
+let node_modules_path = path.resolve(__dirname, '../../../', 'node_modules');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   test: /\.(scss|sass|css)$/i,
   exclude: /\.useable\.(scss|sass|css)$/i,
   use: ExtractTextPlugin.extract({
     fallback: 'style-loader',
-    use: ['css-loader', 'postcss-loader', 'sass-loader']
+    use: [
+      {loader: "css-loader"},
+      // {loader: "postcss-loader"}, // Enable this line cause import problems.
+      {
+        loader: "sass-loader",
+        options: {
+          includePaths: [
+            assets_stylesheets,
+            node_modules_path
+          ]
+        }
+      },
+    ]
   })
-}
+};
