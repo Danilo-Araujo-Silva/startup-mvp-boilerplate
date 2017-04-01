59388fec95e4fbe66ee219bed56e2d6c6e934bcd
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-04-01 20:35:42 -0300

    Optimizing header loading.

--- server/ruby/rails/startup-mvp-boilerplate-rails/app/assets/javascripts/shared/configuration/dependencies/body.ts
+++ server/ruby/rails/startup-mvp-boilerplate-rails/app/assets/javascripts/shared/configuration/dependencies/body.ts
@@ -1,2 +1,74 @@
+import properties from '../properties';
+
 let bodyCSS = require('!style-loader/useable!css-loader!sass-loader!shared/_body.scss');
 bodyCSS.use();
+
+// web - font - icons
+if (properties.isDependencyEnabled('font-awesome')) {
+  let fontAwesomeStyles = require('!style-loader/useable!css-loader!sass-loader!shared/dependencies/font_family/_font_awesome.scss');
+  fontAwesomeStyles.use();
+} else {
+
+}
+
+// web - font - icons
+if (properties.isDependencyEnabled('font-material-icons')) {
+  let materialDesignIconsStyles = require('!style-loader/useable!css-loader!sass-loader!shared/dependencies/font_family/_material_icons.scss');
+  materialDesignIconsStyles.use();
+} else {
+
+}
+
+// web - font - icons
+if (properties.isDependencyEnabled('font-roboto')) {
+  let robotoFontStyles = require('!style-loader/useable!css-loader!sass-loader!shared/dependencies/font_family/_roboto.scss');
+  robotoFontStyles.use();
+} else {
+
+}
+
+// rails
+// Unobtrusive scripting adapter, necessary for routes.
+if (properties.isDependencyEnabled('jquery-ujs')) {
+  let jqueryUJS = require('jquery-ujs');
+} else {
+
+}
+
+// web - ui - material
+if (properties.isDependencyEnabled('material-components-web')) {
+  let materialComponentsWebStyles = require('!style-loader/useable!css-loader!sass-loader!shared/dependencies/ui/material/_material_components_web.scss');
+  materialComponentsWebStyles.use();
+
+  let materialComponentsWebJS = require('material-components-web/dist/material-components-web.js');
+  materialComponentsWebJS.autoInit();
+} else {
+
+}
+
+// rails
+// Unobtrusive scripting adapter, necessary for routes.
+if (properties.isDependencyEnabled('rails-ujs')) {
+  let railsUJS = require('rails-ujs');
+} else {
+
+}
+
+// web - ui - material
+// dependencies: jquery
+if (properties.isDependencyEnabled('semantic-ui')) {
+  let semanticUIStyles = require('!style-loader/useable!css-loader!sass-loader!shared/dependencies/ui/material/_semantic_ui.scss');
+  semanticUIStyles.use();
+
+  let semanticUIJS = require('semantic-ui/dist/semantic.js');
+} else {
+
+}
+
+// rails
+// Rails dependency.
+if (properties.isDependencyEnabled('turbolinks')) {
+  let turbolinks = require('turbolinks');
+}
+
+
--- server/ruby/rails/startup-mvp-boilerplate-rails/app/assets/javascripts/shared/configuration/dependencies/head.ts
+++ server/ruby/rails/startup-mvp-boilerplate-rails/app/assets/javascripts/shared/configuration/dependencies/head.ts
@@ -1,29 +1,5 @@
 import properties from '../properties';
 
-// web - font - icons
-if (properties.isDependencyEnabled('font-awesome')) {
-  let fontAwesomeStyles = require('!style-loader/useable!css-loader!sass-loader!shared/dependencies/font_family/_font_awesome.scss');
-  fontAwesomeStyles.use();
-} else {
-
-}
-
-// web - font - icons
-if (properties.isDependencyEnabled('font-material-icons')) {
-  let materialDesignIconsStyles = require('!style-loader/useable!css-loader!sass-loader!shared/dependencies/font_family/_material_icons.scss');
-  materialDesignIconsStyles.use();
-} else {
-
-}
-
-// web - font - icons
-if (properties.isDependencyEnabled('font-roboto')) {
-  let robotoFontStyles = require('!style-loader/useable!css-loader!sass-loader!shared/dependencies/font_family/_roboto.scss');
-  robotoFontStyles.use();
-} else {
-
-}
-
 // web - utility - multi
 if (properties.isDependencyEnabled('jquery')) {
   let jQuery = require('jquery/dist/jquery');
@@ -34,54 +10,10 @@ if (properties.isDependencyEnabled('jquery')) {
 
 }
 
-// rails
-// Unobtrusive scripting adapter, necessary for routes.
-if (properties.isDependencyEnabled('jquery-ujs')) {
-  let jqueryUJS = require('jquery-ujs');
-} else {
-
-}
-
-// web - ui - material
-if (properties.isDependencyEnabled('material-components-web')) {
-  let materialComponentsWebStyles = require('!style-loader/useable!css-loader!sass-loader!shared/dependencies/ui/material/_material_components_web.scss');
-  materialComponentsWebStyles.use();
-
-  let materialComponentsWebJS = require('material-components-web/dist/material-components-web.js');
-  materialComponentsWebJS.autoInit();
-} else {
-
-}
-
-// rails
-// Unobtrusive scripting adapter, necessary for routes.
-if (properties.isDependencyEnabled('rails-ujs')) {
-  let railsUJS = require('rails-ujs');
-} else {
-
-}
-
-// web - ui - material
-// dependencies: jquery
-if (properties.isDependencyEnabled('semantic-ui')) {
-  let semanticUIStyles = require('!style-loader/useable!css-loader!sass-loader!shared/dependencies/ui/material/_semantic_ui.scss');
-  semanticUIStyles.use();
-
-  let semanticUIJS = require('semantic-ui/dist/semantic.js');
-} else {
-
-}
-
 // utility - ui - positioning
 if (properties.isDependencyEnabled('tether')) {
   let tetherJS = require('tether/dist/js/tether.min.js');
   (<any>window).Tether = (<any>global).Tether = tetherJS;
-}
-
-// rails
-// Rails dependency.
-if (properties.isDependencyEnabled('turbolinks')) {
-  let turbolinks = require('turbolinks');
 }
 
 // web - ui - material
