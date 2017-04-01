7d0e253b57164129228409636f5e6bd3197b9d75
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-04-01 07:29:44 -0300

    Adding very begin initializer and a very end finalizer. Finalizer is not referenced yet so it will not work.

---
+++ server/ruby/rails/startup-mvp-boilerplate-rails/config/finalizers/end.rb
@@ -0,0 +1,6 @@
+# Put your code inside of the END block and it will run
+#  in the very end of the application shutdown.
+# If you put more than one END block they will run in the reserve order.
+END {
+  # Code goes here.
+}
